683bb63f770c607dae45c5f98bfd91e858fbb257
Heisenbuug <deathcoderx@gmail.com>
2020-10-11 00:56:00 +0530

    Draft 1.2

--- app.js
+++ app.js
@@ -1,40 +1,80 @@
 var container = document.getElementById("mainContainer");
-const navBar = `<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
-<a class="navbar-brand" href="#">Navbar</a>
-<button
-  class="navbar-toggler"
-  type="button"
-  data-toggle="collapse"
-  data-target="#navbarNavDropdown"
-  aria-controls="navbarNavDropdown"
-  aria-expanded="false"
-  aria-label="Toggle navigation"
->
-  <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNavDropdown">
-  <ul class="navbar-nav ml-auto">
-    <li class="nav-item">
-      <a class="nav-link" href="#">Sign In</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">About</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Contact Us</a>
-    </li>
-  </ul>
-</div>
-</nav>`;
 
-const grid = 
-container.innerHTML = navBar;
+const skeleton = `<div class="row-12">
+<div class="col p-0">
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+    <a class="navbar-brand" href="#">Blind Watch Maker</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sign In</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <!-- End of Nav Bar -->
+</div>
+</div>
+<!-- Row 1 -->
+<div class="row" id="productGrid">
+
+</div>
+`;
+container.innerHTML = skeleton;
+const pGrid = document.getElementById("productGrid"); 
+function iterateProducts(value, index, array) {
+  const place = document.createElement('div');
+  const card = document.createElement('div');
+  const pImg = document.createElement('img');
+  const cardBody = document.createElement('div');
+  const pName = document.createElement('h5');
+  const pDesc = document.createElement('p');
+  const button = document.createElement('button');
+
+  place.classList.add("col", "mt-3", "mb-3", "col-md-4");
+  card.classList.add("card");
+  pImg.classList.add("card-img-top", "productImage");
+  cardBody.classList.add("card-body");
+  pName.classList.add("card-title");
+  pDesc.classList.add("card-text", "text-muted");
+  button.classList.add("btn", "btn-primary", "mt-1");
+
+  place.appendChild(card);
+  card.appendChild(pImg);
+  card.appendChild(cardBody);
+  cardBody.appendChild(pName);
+  cardBody.appendChild(pDesc);
+  cardBody.appendChild(button);
+
+  pImg.src = array[index]['imgLink'];
+  pName.innerText = array[index]['name'];
+  pDesc.innerText = array[index]['desc'] + ' ' + array[index]['price'];
+  button.innerText = 'Add To Cart'
+  console.log(place);
+  pGrid.appendChild(place);
+}
+
+
 fetch('./products.js')
   .then(res => res.json())
   .then(data => {
-    // console.log(data[0])
-    // var myPara = document.getElementById("myTestPara");
-    // myPara.innerText = data[0].name + ' is our product';
+    data.forEach(iterateProducts);
   })
   .catch(err => console.error(err));
-
--- products.js
+++ products.js
@@ -2,50 +2,59 @@
   {
     "name": "Product One",
     "price": 100000,
-    "imgLink": "https://unsplash.com/photos/VBllXgIhyrs"
+    "imgLink": "https://images.unsplash.com/photo-1575311373937-040b8e1fd5b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "desc": "I am the product!!"
   },
   
   {
     "name": "Product Two",
     "price": 100000,
-    "imgLink": "https://unsplash.com/photos/VBllXgIhyrs"
+    "imgLink": "https://images.unsplash.com/photo-1514218842929-d6b0d653a623?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
+    "desc": "I am the product!!"
   },
   {
     "name": "Product Three",
     "price": 100000,
-    "imgLink": "https://unsplash.com/photos/VBllXgIhyrs"
+    "imgLink": "https://images.unsplash.com/photo-1569917197885-9d2e73bd7430?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "desc": "I am the product!!"
   },
   
   {
     "name": "Product Four",
     "price": 100000,
-    "imgLink": "https://unsplash.com/photos/VBllXgIhyrs"
+    "imgLink": "https://images.unsplash.com/photo-1593019079536-c676b265a577?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "desc": "I am the product!!"
   },
   {
     "name": "Product Five",
     "price": 100000,
-    "imgLink": "https://unsplash.com/photos/VBllXgIhyrs"
+    "imgLink": "https://images.unsplash.com/photo-1593019079561-3dfa82bb25f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "desc": "I am the product!!"
   },
   
   {
     "name": "Product Six",
     "price": 100000,
-    "imgLink": "https://unsplash.com/photos/VBllXgIhyrs"
+    "imgLink": "https://images.unsplash.com/photo-1575054092299-4a300e7a2511?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "desc": "I am the product!!"
   },
   {
     "name": "Product Seven",
     "price": 100000,
-    "imgLink": "https://unsplash.com/photos/VBllXgIhyrs"
+    "imgLink": "https://images.unsplash.com/photo-1530476591165-92859554a03d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "desc": "I am the product!!"
   },
   
   {
     "name": "Product Eight",
     "price": 100000,
-    "imgLink": "https://unsplash.com/photos/VBllXgIhyrs"
+    "imgLink": "https://images.unsplash.com/photo-1534347384071-7f5802bf42a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "desc": "I am the product!!"
   },
   {
     "name": "Product Nine",
     "price": 100000,
-    "imgLink": "https://unsplash.com/photos/VBllXgIhyrs"
+    "imgLink": "https://images.unsplash.com/photo-1486746415817-2c865df52cf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "desc": "I am the product!!"
   }
 ]
